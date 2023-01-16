bf30676062f120f4c5102ea55cd6468a8d981a05
aginga <brianaginga35@gmail.com>
2023-01-16 10:20:45 -0800

    js added

--- dom.js
+++ dom.js
@@ -1,4 +1,40 @@
-//GetElementById()
 
-const title = document.getElementById("main-heading");
-console.log(title);
+//Dom Manupulation
+
+//GetElementsById
+
+const title = document.getElementById('main-heading');
+
+//console.log(title);
+
+//GetElementByClassName()
+
+const listItems = document.getElementsByClassName('list-items1');
+
+//console.log(listItems);
+
+
+//GetElementsByTagName()
+
+const tag = document.getElementsByTagName('li');
+
+//console.log(tag);
+
+//querry selector
+
+const container = document.querySelector('div');
+
+console.log(container);
+
+
+
+
+
+
+
+
+
+
+
+
+
